76ae12bcf7e6d1861c6bee91f9a4d0d6630253c0
Mohamed <mboudjatat17@hotmail.com>
2025-04-18 20:40:31 +0100

    Added EC2 SSH to .drone file

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('👋 Hello from my Dockerized Node app! My name is Mohamed Boudjatat and I am a DevOps Engieer');
+  res.send('👋 Hello from my Dockerized Node app! My name is Mohamed Boudjatat and I am a DevOps Engieer!!!!!!! 👋');
 });
 
 app.listen(80, '0.0.0.0', () => {
